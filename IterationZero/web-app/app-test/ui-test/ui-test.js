92dbc9822b1b986a023606671f764b6b27e8bc1a
hailh <black_cat_hh@yahoo.com>
2013-08-07 08:51:53 +0700

    Xây dựng khung xương di động          i.   Chọn dùng thư viện Seista         ii.   Chọn dùng Sinon

---
+++ IterationZero/web-app/app-test/ui-test/ui-test.js
@@ -0,0 +1,8 @@
+var Harness = Siesta.Harness.Browser.ExtJS;
+
+Harness.configure({
+    title     : 'Sencha UI Test Demo - Application Tests',
+    hostPageUrl: '../../index.gsp'
+});
+
+Harness.start();
---
+++ IterationZero/web-app/index.js
@@ -0,0 +1,9 @@
+﻿Ext.application({
+    name: 'AM',
+    appFolder: 'app',
+    launch: function () {
+        Ext.get('loading').remove();
+        Ext.get('loading-mask').fadeOut({ remove: true });
+    }
+});
+
