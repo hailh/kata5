06084a9ff32eb642e645c083586093b08b0738d8
hailh <black_cat_hh@yahoo.com>
2013-08-13 13:42:50 +0700

    Refactor code iteration 0

--- IterationZero/web-app/app-test/ui-test/specs/01_join_and_stop.js
+++ IterationZero/web-app/app-test/ui-test/specs/01_join_and_stop.js
@@ -1,4 +1,10 @@
 StartTest(function(t) {
+    joinGame(t);
+    clickStopButton(t);
+    hasStoppedAfterClickStopButton(t);
+});
+
+function joinGame(t){
     t.diag('Open new game and stop game');
     var form = Ext.create('AM.view.GameScreen', {
         renderTo : Ext.getBody(),
@@ -7,12 +13,21 @@ StartTest(function(t) {
     });
     form.show();
     t.pass("Game started !");
+}
+
+function clickStopButton(t){
     t.chain([
         { waitFor : 1000 },
-        { action : 'click', target: Ext.getCmp("btnControl") },
+        { action : 'click', target: Ext.getCmp("btnControl") }
+    ]);
+}
+
+function hasStoppedAfterClickStopButton(t){
+    t.chain([
+        { waitFor : 2000 },
         function(next) {
             t.is(Ext.getCmp('txtStatus').text, 'Game stopped !', 'Game stooped after click "Stop" button');
             next();
         }
     ]);
-});
+}
