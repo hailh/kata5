d6a02c55df4b6ab8a83b90a58c208587cfdea555
hailh <black_cat_hh@yahoo.com>
2013-08-15 09:10:29 +0700

    GREEN test 2

--- FullIteration/web-app/app/controller/GameManager.js
+++ FullIteration/web-app/app/controller/GameManager.js
@@ -9,7 +9,19 @@ Ext.define('AM.controller.GameManager', {
         this.control({
             'gameScreen':{
                 btnClick: function(y, x){
-
+                    if(Utils.count % 2 == 0){
+                        if(Ext.getCmp(y + "_" + x).src == "") {
+                            Ext.getCmp(y + "_" + x).setSrc("images/x.png");
+                            Ext.getCmp('txtStatus').setText("X played, O playing ...");
+                            ++Utils.count;
+                        }
+                    } else {
+                        if(Ext.getCmp(y + "_" + x).src == "") {
+                            Ext.getCmp(y + "_" + x).setSrc("images/o.png");
+                            Ext.getCmp('txtStatus').setText("O played, X playing ...");
+                            ++Utils.count;
+                        }
+                    }
                 },
                 btnControlClick: function(){
                     Ext.getCmp('txtStatus').setText("Game stopped !");
